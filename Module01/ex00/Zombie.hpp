b9eb84fbf8f35579628ffdaba99ffb5b97fcabdc
eel-abed <eli_elabed@hotmail.fr>
2025-04-15 18:49:19 +0200

    Add Zombie class implementation with header and source files

--- Module01/ex00/Zombie.hpp
+++ Module01/ex00/Zombie.hpp
@@ -6,7 +6,29 @@
 /*   By: eel-abed <eel-abed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 14:24:49 by eel-abed          #+#    #+#             */
-/*   Updated: 2025/04/15 14:24:50 by eel-abed         ###   ########.fr       */
+/*   Updated: 2025/04/15 18:49:07 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+class Zombie
+{
+	private:
+	std::string name;
+
+	public:
+	Zombie(std::string name);
+	~Zombie();
+
+	void announce(void);
+};
+
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
+
+#endif
