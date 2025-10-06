929205bfa74897337d023c09a51b4f4c8a1994fd
eel-abed <eli_elabed@hotmail.fr>
2025-10-06 16:27:28 +0200

    no comments

--- Module03/ex00/ClapTrap.cpp
+++ Module03/ex00/ClapTrap.cpp
@@ -1,19 +1,16 @@
 #include "ClapTrap.hpp"
 
-// Constructor
 ClapTrap::ClapTrap(const std::string &name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
 {
 	std::cout << "ClapTrap " << _name << " constructor called" << std::endl;
 }
 
-// Copy constructor
 ClapTrap::ClapTrap(const ClapTrap &other) : _name(other._name), _hitPoints(other._hitPoints),
 											_energyPoints(other._energyPoints), _attackDamage(other._attackDamage)
 {
 	std::cout << "ClapTrap " << _name << " copy constructor called" << std::endl;
 }
 
-// Assignment operator
 ClapTrap &ClapTrap::operator=(const ClapTrap &other)
 {
 	std::cout << "ClapTrap assignment operator called" << std::endl;
@@ -27,13 +24,11 @@ ClapTrap &ClapTrap::operator=(const ClapTrap &other)
 	return *this;
 }
 
-// Destructor
 ClapTrap::~ClapTrap()
 {
 	std::cout << "ClapTrap " << _name << " destructor called" << std::endl;
 }
 
-// Attack function
 void ClapTrap::attack(const std::string &target)
 {
 	if (_hitPoints == 0)
@@ -51,7 +46,6 @@ void ClapTrap::attack(const std::string &target)
 	std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!" << std::endl;
 }
 
-// Take damage function
 void ClapTrap::takeDamage(unsigned int amount)
 {
 	if (_hitPoints == 0)
@@ -72,7 +66,6 @@ void ClapTrap::takeDamage(unsigned int amount)
 	}
 }
 
-// Be repaired function
 void ClapTrap::beRepaired(unsigned int amount)
 {
 	if (_hitPoints == 0)
@@ -91,7 +84,6 @@ void ClapTrap::beRepaired(unsigned int amount)
 	std::cout << "ClapTrap " << _name << " is repaired for " << amount << " hit points! Current hit points: " << _hitPoints << std::endl;
 }
 
-// Getters
 std::string ClapTrap::getName() const
 {
 	return _name;
--- Module03/ex00/main.cpp
+++ Module03/ex00/main.cpp
@@ -2,29 +2,21 @@
 
 int main()
 {
-	// Creating a ClapTrap
 	ClapTrap clap("R2D2");
-
-	// Testing attack
 	clap.attack("enemy robot");
 	clap.attack("another target");
 
-	// Testing taking damage
 	clap.takeDamage(5);
-	clap.takeDamage(3);
 
-	// Testing repair
 	clap.beRepaired(2);
 
-	// Testing what happens when no energy
 	ClapTrap tired("GROS_BOT");
 	for (int i = 0; i < 10; i++)
 	{
 		tired.attack("dummy");
 	}
-	tired.attack("should not work");
+	tired.attack("should,T not work");
 
-	// Testing what happens when destroyed
 	ClapTrap weak("NPC");
 	weak.takeDamage(20);
 	weak.attack("ghost");
