dc5a666ef1bf17fbf8ba803c70e88506e84b9876
Elias El Abed <eli_elabed@hotmail.fr>
2026-02-02 19:30:11 +0100

    iter?

---
+++ Module07/ex01/iter.hpp
@@ -0,0 +1,26 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <cstddef>
+
+template <typename T, typename F>
+void iter(T *array, const std::size_t length, F func) {
+    if (!array || length == 0) {
+        return;
+    }
+    for (std::size_t i = 0; i < length; ++i) {
+        func(array[i]);
+    }
+}
+
+template <typename T, typename F>
+void iter(const T *array, const std::size_t length, F func) {
+    if (!array || length == 0) {
+        return;
+    }
+    for (std::size_t i = 0; i < length; ++i) {
+        func(array[i]);
+    }
+}
+
+#endif
---
+++ Module07/ex01/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include "iter.hpp"
+
+static void printInt(const int &value) {
+    std::cout << value << " ";
+}
+
+static void incrementInt(int &value) {
+    ++value;
+}
+
+template <typename T>
+void printValue(const T &value) {
+    std::cout << value << " ";
+}
+
+int main() {
+    int numbers[] = {1, 2, 3, 4, 5};
+    const std::size_t numCount = sizeof(numbers) / sizeof(numbers[0]);
+
+    std::cout << "Original ints: ";
+    iter(numbers, numCount, printInt);
+    std::cout << "\n";
+
+    iter(numbers, numCount, incrementInt);
+
+    std::cout << "Incremented ints: ";
+    iter(numbers, numCount, printValue<int>);
+    std::cout << "\n";
+
+    const std::string words[] = {"hello", "from", "iter"};
+    const std::size_t wordCount = sizeof(words) / sizeof(words[0]);
+
+    std::cout << "Const strings: ";
+    iter(words, wordCount, printValue<std::string>);
+    std::cout << "\n";
+
+    return 0;
+}
