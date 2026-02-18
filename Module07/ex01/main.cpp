c1cceca144107124752674eec9473d5de0af2339
eel-abed <eli_elabed@hotmail.fr>
2026-02-18 15:23:24 +0100

    ti detaui

--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -23,20 +23,20 @@ int main()
 	int numbers[] = {1, 2, 3, 4, 5};
 	const std::size_t numCount = sizeof(numbers) / sizeof(numbers[0]);
 
-	std::cout << "Original ints: ";
+	std::cout << "Original int: ";
 	iter(numbers, numCount, printInt);
 	std::cout << "\n";
 
 	iter(numbers, numCount, incrementInt);
 
-	std::cout << "Incremented ints: ";
+	std::cout << "Incremented int: ";
 	iter(numbers, numCount, printValue<int>);
 	std::cout << "\n";
 
 	const std::string words[] = {"allo", "c'est", "moi"};
 	const std::size_t wordCount = sizeof(words) / sizeof(words[0]);
 
-	std::cout << "Const strings: ";
+	std::cout << "strings: ";
 	iter(words, wordCount, printValue<std::string>);
 	std::cout << "\n";
 
